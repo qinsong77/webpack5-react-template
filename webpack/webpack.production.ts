f3092f0600d0c94d84c731cde24773508dae5632
notend <xyqinsong@163.com>
2024-04-23 15:44:38 +0800

    build: update webpack config

--- webpack/webpack.production.ts
+++ webpack/webpack.production.ts
@@ -15,7 +15,17 @@ const config: Configuration = {
   optimization: {
     minimize: true, //开启压缩
     moduleIds: 'deterministic', //单独模块id，模块内容变化再更新
-    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin({
+        parallel: true, // 开启多线程压缩
+        terserOptions: {
+          compress: {
+            pure_funcs: ['console.log'], // 删除console.log, or drop_console
+          },
+        },
+      }),
+    ],
     runtimeChunk: true,
     splitChunks: {
       chunks: 'all', // 匹配的块的类型：initial（初始块），async（按需加载的异步块），all（所有块）
@@ -24,18 +34,20 @@ const config: Configuration = {
       cacheGroups: {
         // 项目第三方组件
         vendor: {
-          name: 'vendors',
           enforce: true, // ignore splitChunks.minSize, splitChunks.minChunks, splitChunks.maxAsyncRequests and splitChunks.maxInitialRequests
-          test: /[\\/]node_modules[\\/]/,
-          priority: 10,
+          test: /node_modules/, // 只匹配node_modules里面的模块
+          name: 'vendors', // 提取文件命名为vendors,js后缀和chunk hash会自动加
+          minChunks: 1, // 只要使用一次就提取出来
+          chunks: 'initial', // 只提取初始化就能获取到的模块,不管异步的
+          minSize: 0, // 提取代码体积大于0就提取出来
+          priority: 1, // 提取优先级为1
         },
-        // 项目公共组件
-        default: {
-          minSize: 0, // 分离后的最小块文件大小默认3000
-          name: 'common', // 用以控制分离后代码块的命名
-          minChunks: 3, // 最小共用次数
-          priority: 10, // 优先级，多个分组冲突时决定把代码放在哪块
-          reuseExistingChunk: true,
+        commons: {
+          // 提取页面公共代码
+          name: 'commons', // 提取文件命名为commons
+          minChunks: 2, // 只要使用两次就提取出来
+          chunks: 'initial', // 只提取初始化就能获取到的模块,不管异步的
+          minSize: 0, // 提取代码体积大于0就提取出来
         },
       },
     },
